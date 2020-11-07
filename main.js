9b521e03101ea9cb6528be9ec52399e62b9e3e7c
cricket-fan <72508824+cricket-fan@users.noreply.github.com>
2020-11-07 15:44:15 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+Webcam.set({
+    width: 350,
+    height: 300,
+    image_format: 'png',
+    png_qulity: 90
+});
+camera = document.getElementById("camera");
+Webcam.attach('#camera');
+
+function take_snapshot() {
+    Webcam.snap(
+        function(data_uri) {
+            document.getElementById("result").innerHTML = '<img id = "captured_image" src = "' + data_uri + '"/>'
+        }
+    )
+}
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/VKpwz6OFJ/model.json', modelLoaded)
+
+function modelLoaded() {
+    console.log("model done")
+}
